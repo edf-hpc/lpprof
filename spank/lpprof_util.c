5b12987288da2413240fd87412cd89fe541191bc
Pierre Trespeuch <pierre-externe.trespeuch@edf.fr>
2017-12-22 09:36:01 +0100

    Correct spank plugin, sort pids and make sure to get back in job submission directory

--- spank/lpprof_util.c
+++ spank/lpprof_util.c
@@ -6,6 +6,39 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+
+void quicksort( int a[], int l, int r)
+{
+  int j;
+
+  if( l < r )
+    {
+      // divide and conquer
+      j = partition( a, l, r);
+      quicksort( a, l, j-1);
+      quicksort( a, j+1, r);
+    }
+
+}
+
+int partition( int a[], int l, int r) {
+  int pivot, i, j, t;
+  pivot = a[l];
+  i = l; j = r+1;
+
+  while( 1)
+    {
+      do ++i; while( a[i] <= pivot && i <= r );
+      do --j; while( a[j] > pivot );
+      if( i >= j ) break;
+      t = a[i]; a[i] = a[j]; a[j] = t;
+    }
+  t = a[l]; a[l] = a[j]; a[j] = t;
+  return j;
+}
+
+
+
 int slurm_getenv(spank_t sp,char* value,char* env_varname){
 
   char error_msg[512];
@@ -21,7 +54,7 @@ int slurm_getenv(spank_t sp,char* value,char* env_varname){
 int write_pid_file(pid_t pid){
   // Make a file named with current task pid
   char s_pid[64];   // 64 digits for pid should be enought
-  snprintf(s_pid, 1024, "%d",pid);
+  snprintf(s_pid, 64, "%d",pid);
   FILE* pidfile=fopen(s_pid,"w");
   fclose(pidfile);
 
@@ -64,6 +97,7 @@ int count_ranks(const char* rank_list){
   // Use a copy of initial rank_list string for parsing
   char* rank_listw=(char *) malloc(sizeof(char)*strlen(rank_list)+1);
   strncpy(rank_listw,rank_list,strlen(rank_list)+1);
+
     
   // Count number of ranks inside formated lists like : 0,2,5-7,9  
   for (istr = 1, str1 = rank_listw; ;istr++,str1=NULL) {
@@ -74,13 +108,15 @@ int count_ranks(const char* rank_list){
     for (str2 = token; ; str2 = NULL) {
       subtoken = strtok_r(str2,"-", &saveptr2);
       if (subtoken == NULL){
-	if (current_rank>0)
+	if (current_rank>0){
 	    nbranks+=1;
+	}
 	break;
       }
       if (current_rank!=-1){
-	if(atoi(subtoken)-current_rank>0)
-	  nbranks+=atoi(subtoken)-current_rank;
+	if(atoi(subtoken)-current_rank>0){
+	  nbranks+=atoi(subtoken)-current_rank+1;
+	}
 	current_rank=-1;
       }else{
 	current_rank=atoi(subtoken);
@@ -88,18 +124,21 @@ int count_ranks(const char* rank_list){
     }
   }
 
+
   free(rank_listw);
   return nbranks;
 }
 
 
-int read_pids(char** pid_list){
+int read_pids(char** pid_list,int nbpids){
 
   int first=1;
   int len_entry=0;
   DIR * dir=NULL;
   struct dirent * buf=NULL, * de=NULL;
-
+  int ipid=0;
+  int* ipid_list=(int*) malloc(nbpids*sizeof(int));
+  
   
   if ((dir = opendir("."))
       && (len_entry = offsetof(struct dirent, d_name) + fpathconf(dirfd(dir), _PC_NAME_MAX) + 1)
@@ -109,20 +148,27 @@ int read_pids(char** pid_list){
 	{
 	  if (de->d_type == DT_REG)
 	    {
-	      if(first){
-		first=0;
-	      }else{
-		strcat(*pid_list,",");
-	      }
-	      strcat(*pid_list,de->d_name);
-	      
+	      ipid_list[ipid]=atoi(de->d_name);
+	      ipid+=1;
 	    }
 	}
     }
   else{
     return(-1);
   }
+
+  quicksort(ipid_list,0,nbpids-1);
+
+  char pid_buffer[64]; // 64 should be enought to contain a pid
+  for(ipid=0;ipid<nbpids;ipid++){
+    snprintf(pid_buffer,64, "%d", ipid_list[ipid]);
+    strcat(*pid_list,pid_buffer);
+    if (ipid!=nbpids-1)
+      strcat(*pid_list,",");
+  }
   
+  
+  free(ipid_list);
   free(buf);
 
   return(0);
--- spank/lpprof_util.h
+++ spank/lpprof_util.h
@@ -10,9 +10,11 @@
 #define FREQUENCY_NOT_SET -1
 #define SLURM_ENVSIZE 8192
 
+void quicksort( int a[], int l, int r);
+int partition( int a[], int l, int r);
 int slurm_getenv(spank_t sp,char* value,char* env_varname);
 int write_pid_file(pid_t pid);
 int count_pid_files();
-int read_pids(char** pid_list);
+int read_pids(char** pid_list,int nbpids);
 
 #endif // LPPROF_UTIL_H_
